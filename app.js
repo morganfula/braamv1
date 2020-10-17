a8214cbd5be84e83e86d80598ed502d7e832b459
morganfula <morgan.fula@me.com>
2020-10-17 20:29:43 +0200

    slide js

---
+++ app.js
@@ -0,0 +1,27 @@
+TweenMax.from('nav', 1.6, {
+  delay: 0.8,
+  opacity: 0,
+  y: -20,
+  ease: Expo.Out
+});
+
+TweenMax.from('.section1', 2.5, {
+  delay: 1.2,
+  opacity: 0,
+  x: -50,
+  ease: Expo.Out
+});
+
+TweenMax.from('.intro-img', 3, {
+  delay: 0.8,
+  opacity: 0,
+  x: 50,
+  ease: Expo.Out
+});
+
+TweenMax.from('.booking-list', 4, {
+  delay: 1,
+  opacity: 0,
+  y: 20,
+  ease: Expo.Out
+});
